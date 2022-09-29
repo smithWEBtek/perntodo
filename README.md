64ca098da86d75016cd49ecb196507560fbc2033
brad@smithwebtek.com <brad@smithwebtek.com>
2022-09-29 07:52:45 -0400

    Add server basics

---
+++ README.md
@@ -0,0 +1,2 @@
+this repo follows this tutorial:
+https://www.youtube.com/watch?v=X8gr-qA_JEs
---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('./db');
+
+// middleware
+app.use(cors);
+app.use(express.json());// req.body
+
+// routes
+
+// create a todo
+app.post('/todos', async(req, res) =>{
+  try {
+    console.log('req.body: ', req.body);
+  } catch (error) {
+    console.log('error: ', error.message);
+  }
+})
+
+// get all todos
+
+// get a todo
+
+// update a todo
+
+// delete a todo
+
+
+app.listen(5000, () => {
+  console.log('server has started on port 5000');
+});
+
